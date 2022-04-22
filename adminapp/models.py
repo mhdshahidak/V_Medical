62528df7e0b3f06526dde57a96d1f2086c96aa3d
shahidbaabtra <mhdshd.ak@gmail.com>
2022-04-22 11:45:18 +0530

    admin models

--- adminapp/models.py
+++ adminapp/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+class AdminLogin(models.Model):
+
+    id = models.AutoField(primary_key=True)
+    username = models.CharField(max_length=100)
+    password = models.CharField(max_length=50)
+
+    class Meta:  # to set table name
+        db_table = "adminlogin"
+
--- v_med/settings.py
+++ v_med/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 import os
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-96#4&!h!bwz*fxe!kkqa0$1c$vmy)h!74&5katg+eqac4yik!&'
+SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -81,11 +82,11 @@ WSGI_APPLICATION = 'v_med.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'v_med',
-        'USER':'postgres',
-        'PASSWORD':'root',
-        'HOST':'127.0.0.1',
+        "ENGINE": config("DB_ENGINE"),
+        "NAME": config("DB_NAME"),
+        "USER": config("DB_USER"),
+        "PASSWORD": config("DB_PASSWORD"),
+        "HOST": config("DB_HOST"),
         'PORT':'5432',
         
     }
